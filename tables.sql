feec9611b6ff01941a2293d45e0196da05227119
kasi2218 <mskasim.1993@gmail.com>
2021-08-29 10:56:10 +0530

    date filed type change

--- tables.sql
+++ tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `employee_details` (
   `emp_name` varchar(255) NOT NULL COMMENT 'employee name',
   `emp_code` varchar(100) NOT NULL COMMENT 'employee code',
   `emp_department` varchar(100) NOT NULL COMMENT 'employee code',
-  `emp_dob` int(11) NOT NULL COMMENT 'employee dob',
-  `emp_doj` int(11) NOT NULL COMMENT 'employee joining date',
+  `emp_dob` date NOT NULL COMMENT 'employee dob',
+  `emp_doj` date NOT NULL COMMENT 'employee joining date',
   PRIMARY KEY (`emp_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
